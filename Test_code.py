42884e34b1f9ed205a692a7c5153ff7c0324f0f7
stranniebudni <neilbv007@gmail.com>
2025-11-23 13:36:57 +0100

    add test code

---
+++ Test_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 23 13:24:40 2025
+
+@author: strannie_budni
+"""
+
+def func():
+    print("Hello world")
+
+
+
+if __name__ == "__main__":
+    func()
